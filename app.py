dd24aa6260b5376487e80bcdbb3cdca3e396d3fc
FelixYale <dexteryale@gmail.com>
2023-03-02 00:00:59 +0100

    feat: create edit endpoint and make fixes

--- app.py
+++ app.py
@@ -2,12 +2,17 @@ from flask import Flask, request, abort, jsonify
 from models import Students, setup_db
 from flask_cors import CORS
 from passlib.hash import pbkdf2_sha256
+import jwt
+from datetime import datetime, timedelta
+from recognition_functions import findEncodings
 
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__)
     setup_db(app)
     CORS(app)
+    with app.app_context():
+        app.run()
 
     @app.after_request
     def after_request(response):
@@ -19,16 +24,24 @@ def create_app(test_config=None):
         )
         return response
 
-    @app.route('/student/register', method=['POST'])
+    @app.route('/student/register', methods=['POST'])
     def register():
         body = request.get_json()
+        # print(body)
+        print(request)
 
-        new_firstname=body.get("firstName", None)
-        new_lastname=body.get("lastName", None)
+        new_firstname=body.get("firstname", None)
+        new_lastname=body.get("lastname", None)
         new_email=body.get("email", None)
         new_password = body.get("password", None)
-        new_matricno = body.get('matricno', None)
-        new_level = body.get('level', None)
+        new_matricno = body.get("matricno", None)
+        new_level = body.get("level", None)
+
+       
+        new_encodings ={}
+        
+
+
 
         existing_user = Students.query.filter_by(email=new_email).first()
         if existing_user is not None:
@@ -39,16 +52,18 @@ def create_app(test_config=None):
         try:
             student = Students(firstname=new_firstname, lastname=new_lastname, email=new_email, matricno=new_matricno, level=new_level, password=hashed_password) 
             student.insert()
-            selection = Students.query.order_by(Students.id).all()
+            # selection = Students.query.order_by(Students.id).all()
 
             return jsonify({
                 "success":True,
-                "created": Students.id
+                "created": Students.id,
+                "Total Students": len(Students.query.all())
             })
-        except:
+        except Exception as e:
+            print(e)
             abort(422)
 
-    @app.route('/student/login', method=['GET'])
+    @app.route('/student/login', methods=['POST'])
     def Studentlogin():
          body = request.get_json()
 
@@ -61,9 +76,48 @@ def create_app(test_config=None):
               return jsonify(message="User does not exist or email is invalid"), 401
          
          if pbkdf2_sha256.verify(new_password, student.password):
-              return jsonify(message="success")
+            payload = {
+              'matricno': new_matric,
+              'exp': datetime.utcnow() + timedelta(minutes=30)  # token expires in 30 minutes
+          }
+            token = jwt.encode(payload, 'secret_key', algorithm='HS256')
+            return jsonify({
+              'message': 'success',
+              'id':student.id,
+              'token': token
+                })
+              
          else:
               return jsonify(message="Invalid email or password"), 401
+         
+    @app.route('/student/create_encodings/<int:id>', methods=['PATCH'])
+    def create_encodings(id):
+         student =Students.query.filter_by(id=id).first()
+
+         file = request.files['file']
+         file_content = file.read()
+
+
+         if student is None:
+            return jsonify(message="Student not found"), 404
+         
+         if 'encodings' not in request.json:
+             return jsonify(message="Request body must contain encodings"), 400
+         
+         new_encodings = findEncodings(file_content)
+         
+
+         try: 
+              student = Students(encodings=new_encodings)
+              student.update()
+              return jsonify(message="Students encoding updated sucessfully"), 200
+         except Exception as e:
+              print(e)
+              abort(422)
+         
+        
+    
+        
 
 
 
@@ -73,7 +127,7 @@ def create_app(test_config=None):
         return jsonify({
                 "success": False,
                 "error": 400,
-                "message": "Bad request(Wrong Input)"
+                "message": "Bad request(Wrong Input)" + str(error)
             }), 400
 
     @app.errorhandler(404)
@@ -89,7 +143,9 @@ def create_app(test_config=None):
             return jsonify({
                 "success": False,
                 "error": 422,
-                "message": 'Unable to Process your request'
+                "message": 'Unable to Process your request' + str(error)
             }), 422
     
     return app
+
+
