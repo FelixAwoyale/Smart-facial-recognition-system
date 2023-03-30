492011a187838ae5730659e20a4cbd226fabf3eb
FelixYale <dexteryale@gmail.com>
2023-03-30 13:17:49 +0100

    feat: create color theme

---
+++ frontend/src/theme.js
@@ -0,0 +1,14 @@
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#008A84",
+      light: "#00ACA5",
+    },
+    secondary: {
+      main: "#6BBD74",
+      light: "#A2F6A8",
+    },
+  },
+});
