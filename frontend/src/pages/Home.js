42cf15bedf3ae479e93bed4a4b7f04b79d4f45b2
FelixYale <dexteryale@gmail.com>
2023-04-17 00:04:39 +0100

    fix: adjust font size and margin

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,26 +1,30 @@
-import { Typography, Container, Stack, Box, Button, Grid } from "@mui/material";
+import { Typography, Container, Box, Button, Grid } from "@mui/material";
 import React from "react";
 import HeroBg from "../assets/Images/herobg2.png";
 
 function Home() {
   return (
-    <div style={{ marginTop: "6em" }}>
+    <div style={{ marginTop: "7em" }}>
       <Container>
         <Grid container>
           <Grid Item md={6}>
             <Box>
               <Typography
-                variant="h3"
-                sx={{ marginTop: "1em", marginBottom: "0.5em" }}
+                variant="h2"
+                sx={{ marginTop: "1em", marginBottom: "1em" }}
               >
                 Smart Facial recognition system for attendance
               </Typography>
-              <Typography variant="p" sx={{ fontSize: "0.8em" }}>
+              <Typography
+                variant="p"
+                sx={{ fontSize: "1em", marginBottom: "1em" }}
+                className="mysubtext"
+              >
                 Smart facial recognition technology for attendance is a system
                 that uses artificial intelligence to recognize and verify an
                 individual's identity through their facial features.
               </Typography>
-              <Box sx={{ marginTop: "1em", marginBottom: "1em" }}>
+              <Box sx={{ marginTop: "2em", marginBottom: "1em" }}>
                 <Button variant="contained">Get Started</Button>
               </Box>
             </Box>
