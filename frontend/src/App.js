81ca72e2938093abe8e50f00f95cca78db22557c
FelixYale <dexteryale@gmail.com>
2023-04-17 00:09:14 +0100

    feat: add new routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,10 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Navbar from "./components/Navbar";
+import HomeDashboard from "./pages/HomeDashboard";
+import LecturerLogin from "./pages/LecturerLogin";
+import LecturerRegister from "./pages/LecturerRegister";
+import LecturerDashboard from "./pages/LecturerDashboard";
 
 function App() {
   return (
@@ -10,8 +14,14 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
+
+        <Route path="/auth/login" element={<Login />} />
+        <Route path="/auth/register" element={<Register />} />
+        <Route path="/auth/lecturer/login" element={<LecturerLogin />} />
+        <Route path="/auth/lecturer/register" element={<LecturerRegister />} />
+
+        <Route path="/student/dashboard" element={<HomeDashboard />} />
+        <Route path="/lecturer/dashboard" element={<LecturerDashboard />} />
       </Routes>
     </>
   );
