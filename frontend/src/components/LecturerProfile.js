6319bc363c2226891bde6923063b70024325524b
FelixYale <dexteryale@gmail.com>
2023-05-24 23:12:09 +0100

    feat: fetch lecturer details from db

--- frontend/src/components/LecturerProfile.js
+++ frontend/src/components/LecturerProfile.js
@@ -1,16 +1,19 @@
 import { Avatar, Box, Typography } from "@mui/material";
 import React from "react";
+import { useAuth } from "../context/AuthContext";
 
 export default function StudentProfile() {
+  const { LecturerState } = useAuth();
   return (
     <div>
       <Box className="studentprofile">
         <Avatar className="mb-profile" />
         <Typography variant="h6" className="mb-profile">
-          Felix Awoyale
+          {LecturerState.name}
         </Typography>
         <Box className="mb-profile" sx={{ marginTop: "1em" }}>
-          <Typography>Email: dexteryale@gmail.com</Typography>
+          <Typography>Email: {LecturerState.email}</Typography>
+          <Typography>Department: {LecturerState.department}</Typography>
           {/* <Typography>Level: 500</Typography> */}
         </Box>
       </Box>
