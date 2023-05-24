122503eec68230c682ceebc22f7da926ec9f5d1c
FelixYale <dexteryale@gmail.com>
2023-05-24 23:12:48 +0100

    feat: fetch students  details from db

--- frontend/src/components/StudentProfile.js
+++ frontend/src/components/StudentProfile.js
@@ -1,17 +1,20 @@
 import { Avatar, Box, Typography } from "@mui/material";
 import React from "react";
+import { useAuth } from "../context/AuthContext";
 
 export default function StudentProfile() {
+  const { UserState } = useAuth();
+  // console.log(UserState);
   return (
     <div>
       <Box className="studentprofile">
         <Avatar className="mb-profile" />
         <Typography variant="h6" className="mb-profile">
-          Felix Awoyale
+          {UserState.name}
         </Typography>
         <Box className="mb-profile" sx={{ marginTop: "1em" }}>
-          <Typography>Matric No: 160407011</Typography>
-          <Typography>Level: 500</Typography>
+          <Typography>Matric No: {UserState.matricno}</Typography>
+          <Typography>Level: {UserState.level}</Typography>
         </Box>
       </Box>
     </div>
